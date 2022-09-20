e846a9e72e1ce3fff28eee7b989edf682ab229ca
rerecum <kacperkacperkacper132@gmail.com>
2022-09-20 14:07:29 +0200

    random

--- PLIKI_TEKSTOWE/Form1.Designer.cs
+++ PLIKI_TEKSTOWE/Form1.Designer.cs
@@ -76,6 +76,8 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(258, 23);
             this.textBox2.TabIndex = 3;
+            this.textBox2.UseSystemPasswordChar = true;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // label3
             // 
--- PLIKI_TEKSTOWE/Form1.cs
+++ PLIKI_TEKSTOWE/Form1.cs
@@ -9,17 +9,36 @@ namespace PLIKI_TEKSTOWE
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
+            if (textBox1.Text == "admin")
+                ;
 
         }
 
         private void label5_Click(object sender, EventArgs e)
         {
-
+            string[] paths = Directory.GetFiles(@"C:\Users\student\Desktop\zadanie", "*.png");
+            List<string> images = paths.ToList();
+            Random random = new Random();
+            pictureBox1.ImageLocation = paths[random.Next(0, images.Count - 1)];
+            if (pictureBox1.ImageLocation.Contains("*.png"))
+            {
+                
+            }
+            else
+            {
+                
+            }
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
+            
+        }
 
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "Qwerty1@34")
+                ;
         }
     }
 }
